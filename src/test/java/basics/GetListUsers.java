f77bbaa28199c7294755b7db884de04922d6cba6
= <=>
2023-03-17 10:28:00 +0530

    Basic validation

--- src/test/java/basics/GetListUsers.java
+++ src/test/java/basics/GetListUsers.java
@@ -1,9 +1,11 @@
 package basics;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+
 
 import static io.restassured.RestAssured.*;
 
@@ -30,11 +32,21 @@ Response response = given()
 					.response();
 
 
-			String stringresponse = response.asPrettyString();
+			String stringresponse = response.asPrettyString();// converting the response into string format
 
 		System.out.println(stringresponse);
 		
 		
+		int statuscode = response.getStatusCode();// returning the status code of the response
+		
+		
+		System.out.println("status code is :"+statuscode);
+		
+		long responsetime = response.getTime();
+		
+		System.out.println("The response time is "+responsetime);
+		
+		Assert.assertEquals(201, statuscode);
 		
 	}
 	
