144fa5530256a0659b54e48c22d281830dae1d67
= <=>
2023-03-17 08:49:58 +0530

    committing for the first time

---
+++ src/test/java/basics/GetListUsers.java
@@ -0,0 +1,44 @@
+package basics;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+public class GetListUsers {
+	
+	
+	
+	@Test
+	public void getListOfUsers()
+	{
+		
+		RestAssured.baseURI = "https://reqres.in";
+		
+Response response = given()
+		
+					.when()
+		
+					.get("api/users?page=2")
+		
+					.then()
+		
+					.extract()
+		
+					.response();
+
+
+			String stringresponse = response.asPrettyString();
+
+		System.out.println(stringresponse);
+		
+		
+		
+	}
+	
+	
+	
+
+}
